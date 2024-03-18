43225f987457b4f17dd110d1a2b5c5b4a80e72ba
Dorjoo <90739633+dorjoohover@users.noreply.github.com>
2024-03-18 17:03:01 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app)
-  await app.listen(4000);
+  await app.listen(5050, 'localhost');
 }
 bootstrap();

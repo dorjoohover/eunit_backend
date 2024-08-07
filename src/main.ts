9ecde8138721690dafa26627b648bce46df4fb93
dorjoo <dorjoohover@gmail.com>
2024-08-07 18:11:46 +0800

    a'a'

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app)
-  // await app.listen(3000);
-  await app.listen(5050);
+  await app.listen(3000);
+  // await app.listen(5050);
 }
 bootstrap();

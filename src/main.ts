7d888d9e0ff2d36cbac51c6b5756691a5eee2bde
dorjoo <dorjoohover@gmail.com>
2024-12-12 16:11:54 +0800

    'a'

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   setupSwagger(app);
-  await app.listen(4000);
+  await app.listen(5000);
 }
 bootstrap();

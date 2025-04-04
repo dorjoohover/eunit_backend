4b17b11e47d790d5884acb3c6eadfd11c22a9c78
dorjoo <dorjoohover@gmail.com>
2025-04-04 10:41:42 +0800

    a

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
         credential: admin.credential.cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         }),
       });
     } else {

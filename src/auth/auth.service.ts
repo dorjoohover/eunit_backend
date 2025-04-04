9198269bcf6fb321513ce2205861ca99169b4d26
dorjoo <dorjoohover@gmail.com>
2025-04-04 10:42:58 +0800

    a

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,10 +22,11 @@ export class AuthService {
     private configService: ConfigService,
   ) {
     if (!admin.apps.length) {
+      console.log(process.env.FIREBASE_PROJECT_ID);
       this.app = admin.initializeApp({
         credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          projectId: process.env.FIREBASE_PROJECT_ID as string,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
           privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         }),
       });

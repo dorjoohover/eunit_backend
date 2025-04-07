a1c6aef2427a801343380e98af6ffe866298f62c
dorjoo <dorjoohover@gmail.com>
2025-04-07 13:07:04 +0800

    a

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,18 +19,19 @@ export class AuthController {
   constructor(private readonly service: AuthService) {}
   @Get('phone-login')
   async phoneLogin(@Request() { user }) {
-    if (!user.data.registered) {
-      console.log(user['phone'] ?? user['email'], user['email']);
-      await this.service.register(
-        {
-          email: user['email'] ?? null,
-          phone: user['phone'] ?? null,
-          lastname: user['name'] ?? null,
-          firstname: null,
-        },
-        user['phone'] ?? user['email'],
-      );
-    }
+    console.log(user)
+    // if (!user.data.registered) {
+      // console.log(user['phone'] ?? user['email'], user['email']);
+      // await this.service.register(
+      //   {
+      //     email: user['email'] ?? null,
+      //     phone: user['phone'] ?? null,
+      //     lastname: user['name'] ?? null,
+      //     firstname: null,
+      //   },
+      //   user['phone'] ?? user['email'],
+      // );
+    // }
     return user;
   }
 
